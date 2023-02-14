1fc7a47156228c1cfc733083bf27584fb0ccd51c
Lowell Alleman <lowell.alleman@cdillc.com>
2023-02-14 16:14:07 -0500

    Bump version: 0.14.2 → 0.15.0

--- plugins/module_utils/ksconf_shared.py
+++ plugins/module_utils/ksconf_shared.py
@@ -11,7 +11,7 @@ from ansible.module_utils.basic import AnsibleModule
 from ansible.module_utils.common.text.converters import to_native
 
 
-__version__ = "0.14.2"
+__version__ = "0.15.0"
 
 SIDELOAD_STATE_FILE = ".ksconf_sideload.json"
 
