ec2a0c5af371ccf0c7c1dae0c1dbe0b718e0d571
Lowell Alleman <lowell.alleman@cdillc.com>
2023-08-25 18:49:59 -0400

    ksconf_app_sideload: Fix bug triggered by non-existent paths

--- plugins/modules/ksconf_app_sideload.py
+++ plugins/modules/ksconf_app_sideload.py
@@ -319,7 +319,9 @@ def ksconf_sideload_app(src, dest, src_orig=None):
         json.dump(data, marker_f, indent=1)
 
     # Inventory paths are relative to the `dest` directory
-    file_list = [os.fspath(app_manifest.name / f.path) for f in app_manifest.files]
+    file_list = [app_manifest.name / f.path for f in app_manifest.files]
+    # Remove non-existent paths (because manifest can be incorrect)
+    file_list = [os.fspath(f.path) for f in file_list if (dest/f).is_file()]
 
     # Hard code this for now!
     result["changed"] = True
