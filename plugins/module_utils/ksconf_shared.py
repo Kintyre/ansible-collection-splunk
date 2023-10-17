f57a94b8f2383455599f9bc124d9aa4e3ae5cb12
Lowell Alleman <lowell.alleman@cdillc.com>
2023-10-17 12:58:38 -0400

    Bump version: 0.22.2 → 0.23.0

--- plugins/module_utils/ksconf_shared.py
+++ plugins/module_utils/ksconf_shared.py
@@ -13,7 +13,7 @@ from random import randint
 from ansible.module_utils.basic import AnsibleModule
 
 
-__version__ = "0.22.2"
+__version__ = "0.23.0"
 
 SIDELOAD_STATE_FILE = ".ksconf_sideload.json"
 
